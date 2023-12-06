cf0d8c24ef701e476966d0c5de8e2950a13deff6
James Lakin <james@jameslakin.co.uk>
2023-12-06 18:02:15 +0000

    Add blob path

--- pkg/mux/mux.go
+++ pkg/mux/mux.go
@@ -6,18 +6,19 @@ import (
 	"github.com/gorilla/mux"
 )
 
-// Based off the result of remoteName from https://github.com/distribution/distribution/
+// Based off the result of remoteName from https://github.com/distribution/distribution's regexp.go
 const imageNamePattern = "[a-z0-9]+(?:(?:[._]|__|[-]+)[a-z0-9]+)*(?:/[a-z0-9]+(?:(?:[._]|__|[-]+)[a-z0-9]+)*)*"
 
 type Service interface {
-	GetManifest(repo string, ref string, registry string) string
+	GetManifest(repo string, ref string, registry string, headers *http.Header, w http.ResponseWriter)
+	GetBlob(repo string, digest string, registry string, headers *http.Header, w http.ResponseWriter)
 }
 
 func NewRouter(services Service) *mux.Router {
 	r := mux.NewRouter()
 
 	r.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("Hello world"))
+		w.Write([]byte("cacheyd"))
 	})
 
 	r.HandleFunc("/v2/{repo:"+imageNamePattern+"}/manifests/{ref}", func(w http.ResponseWriter, r *http.Request) {
@@ -28,9 +29,25 @@ func NewRouter(services Service) *mux.Router {
 		if registry == "" {
 			w.WriteHeader(400)
 			w.Write([]byte("No ns query string given (are you using containerd?): I don't know what registry to ask for " + repo))
+			return
 		}
 
-		w.Write([]byte(services.GetManifest(repo, vars["ref"], registry)))
+		services.GetManifest(repo, vars["ref"], registry, &r.Header, w)
+	})
+
+	// I assume registries ensure a form of SHA hash here, but let's not care about that.
+	r.HandleFunc("/v2/{repo:"+imageNamePattern+"}/blobs/{digest}", func(w http.ResponseWriter, r *http.Request) {
+		vars := mux.Vars(r)
+		repo := vars["repo"]
+		registry := r.URL.Query().Get("ns")
+
+		if registry == "" {
+			w.WriteHeader(400)
+			w.Write([]byte("No ns query string given (are you using containerd?): I don't know what registry to ask for " + repo))
+			return
+		}
+
+		services.GetBlob(repo, vars["digest"], registry, &r.Header, w)
 	})
 
 	return r
--- pkg/mux/mux_test.go
+++ pkg/mux/mux_test.go
@@ -11,8 +11,10 @@ import (
 
 type noOpService struct{}
 
-func (s *noOpService) GetManifest(repo string, ref string, registry string) string {
-	return ""
+func (s *noOpService) GetManifest(repo string, ref string, registry string, headers *http.Header, w http.ResponseWriter) {
+	w.Write([]byte("{}"))
+}
+func (s *noOpService) GetBlob(repo string, digest string, registry string, headers *http.Header, w http.ResponseWriter) {
 }
 
 func TestManifestsPaths(t *testing.T) {
@@ -46,6 +48,57 @@ func TestManifestsPaths(t *testing.T) {
 		// Missing ns
 		{
 			url:    "/v2/prom/node-exporter/manifests/v1.5.0",
+			expect: 400,
+		},
+	}
+
+	r := NewRouter(&noOpService{})
+
+	for _, tC := range testCases {
+		t.Run(strings.ReplaceAll(tC.url, "/", "-"), func(t *testing.T) {
+			req, err := http.NewRequest("GET", tC.url, nil)
+			if err != nil {
+				t.Fatal(err)
+			}
+			rr := httptest.NewRecorder()
+			r.ServeHTTP(rr, req)
+
+			assert.Equal(t, tC.expect, rr.Code)
+		})
+	}
+}
+
+func TestBlobPaths(t *testing.T) {
+	testCases := []struct {
+		url    string
+		expect int
+	}{
+		{
+			url:    "/v2/prom/node-exporter/blobs/sha256:b7b28af77ffec6054d13378df4fdf02725830086c7444d9c278af25312aa39b9?ns=docker.io",
+			expect: 200,
+		},
+		{
+			url:    "/v2/somebody/prom/node-exporter/blobs/sha256:b7b28af77ffec6054d13378df4fdf02725830086c7444d9c278af25312aa39b9?ns=docker.io",
+			expect: 200,
+		},
+		{
+			url:    "/v2/node-exporter/blobs/sha256:b7b28af77ffec6054d13378df4fdf02725830086c7444d9c278af25312aa39b9?ns=docker.io",
+			expect: 200,
+		},
+
+		// Missing ref or v2
+		{
+			url:    "/v2/prom/node-exporter/blobs?ns=docker.io",
+			expect: 404,
+		},
+		{
+			url:    "/prom/node-exporter/blobs/sha256:b7b28af77ffec6054d13378df4fdf02725830086c7444d9c278af25312aa39b9?ns=docker.io",
+			expect: 404,
+		},
+
+		// Missing ns
+		{
+			url:    "/v2/prom/node-exporter/blobs/sha256:b7b28af77ffec6054d13378df4fdf02725830086c7444d9c278af25312aa39b9",
 			expect: 400,
 		},
 	}
