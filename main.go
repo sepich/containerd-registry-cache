51ee63e0446e43d3ea2cadaf3166439def25d0f6
Alex R <alex@ryabov.dev>
2025-07-15 14:56:19 +0200

    reduce prod logs

--- main.go
+++ main.go
@@ -106,7 +106,7 @@ func getLogger(logLevel string) *slog.Logger {
 	}
 	return slog.New(slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{
 		Level:     l,
-		AddSource: true,
+		AddSource: logLevel == "debug",
 		ReplaceAttr: func(groups []string, a slog.Attr) slog.Attr {
 			if a.Key == slog.TimeKey && len(groups) == 0 {
 				return slog.Attr{}
